10c5c15bbeb54b4f13647e187333dc4887aac055
Your Name <you@example.com>
2019-01-13 15:20:41 -0800

    Renaming to a more appropriate variable.

--- js/main.js
+++ js/main.js
@@ -6,11 +6,11 @@ let btnMoveDown = document.getElementsByClassName("move-down")[0];
 let messageContainer = document.getElementsByClassName("message")[0];
 let avatar = document.getElementsByClassName("avatar")[0];
 
-let xAxis = 30;
-let yAxis = 30;
+let currentX = 30;
+let currentY = 30;
 let isMovable = true;
 let isCollision = false;
-const stepsReset = 30;
+const resetSteps = 30;
 const numOfSteps = 30;
 
 
@@ -34,7 +34,7 @@ let check = {
         isMovable = n;
     },
     getCollision: (checkSteps, checkAxis) => {
-        newAxis = checkAxis === move.setXAxis ? xAxis : yAxis;
+        newAxis = checkAxis === move.setXAxis ? currentX : currentY;
         let newSteps = newAxis + checkSteps;
         isCollision = newSteps < 0 ? true : false;
         return isCollision;
@@ -45,15 +45,15 @@ let check = {
 }
 let move = {
     respawn: (n) => {
-        render.setXAxis(xAxis = n);
-        render.setYAxis(yAxis = n);
+        render.setXAxis(currentX = n);
+        render.setYAxis(currentY = n);
         error.resetMessage();
     },
     setXAxis: (n) => {
-        render.setXAxis(xAxis += n);
+        render.setXAxis(currentX += n);
     },
     setYAxis: (n) => {
-        render.setYAxis(yAxis += n);
+        render.setYAxis(currentY += n);
     }
 }
 let render = {
@@ -81,7 +81,7 @@ let error = {
 
 
 btnRespawn.addEventListener("click", () => {
-    move.respawn(stepsReset);
+    move.respawn(resetSteps);
 });
 btnMoveLeft.addEventListener("click", () => {
     check.movingProcess(-numOfSteps, move.setXAxis);
